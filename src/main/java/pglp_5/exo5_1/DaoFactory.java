6b0082c1bce4b13d59e537b23dbff3f4f21ee0ed
melissa bafdel <melissabafdel@gmail.com>
2020-04-07 23:41:08 +0200

    ajout de classes

--- src/main/java/pglp_5/exo5_1/DaoFactory.java
+++ src/main/java/pglp_5/exo5_1/DaoFactory.java
@@ -1,5 +1,20 @@
 package pglp_5.exo5_1;
 
-public class DaoFactory {
+import java.io.IOException;
 
+public class DaoFactory {
+	
+	//recuperer le numero de telephone
+	public static DAOApp<NumeroTel> getNumeroTelDAO() throws IOException{
+		return new NumeroTelDAO();}
+		
+		//recuperer de DAO personnel
+		public static DAOApp<Personnel> getPersonnelDAO(){ 
+			return new PersonnelDAO();
+		}
+	
+	//recuperer le DAO de GroupePersonnel
+		 public static DAO<GroupePersonnel> getGroupePersonnels() {
+		        return new GroupePersonnelsDAO();
+		    }
 }
---
+++ src/main/java/pglp_5/exo5_1/NumeroTelDAO.java
@@ -0,0 +1,32 @@
+package pglp_5.exo5_1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class NumeroTelDAO extends DAOApp<NumeroTel> {
+
+	@Override
+	public NumeroTel create(NumeroTel obj) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(NumeroTel obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public NumeroTel update(NumeroTel obj) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NumeroTel recherche(int id) throws FileNotFoundException, ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
