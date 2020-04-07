be9bdae3f5e662b25d4d4a216ec7f5281cd24b95
melissa bafdel <melissabafdel@gmail.com>
2020-04-07 23:24:09 +0200

    ajout de classes

--- src/main/java/pglp_5/exo5_1/DAOApp.java
+++ src/main/java/pglp_5/exo5_1/DAOApp.java
@@ -1,8 +1,12 @@
 package pglp_5.exo5_1;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public abstract class DAOApp<T> {
@@ -16,4 +20,33 @@ public abstract class DAOApp<T> {
     	fileOut = new FileOutputStream(f);
     	objOut = new ObjectOutputStream(fileOut);
     }
+    
+    //creation
+    public abstract T create (T obj) throws IOException;
+    
+    //effacer
+    public abstract void delete(T obj);
+    
+    //mise a jour
+    public abstract T update(T obj) throws IOException;
+    
+    //recherche de l'information
+    public abstract T recherche(int id) throws FileNotFoundException, ClassNotFoundException, IOException;
+    
+    //déserialisation
+    public Object deserialize(final byte[] bytes) throws ClassNotFoundException, IOException {
+        ByteArrayInputStream a = new ByteArrayInputStream(bytes);
+        ObjectInputStream b = new ObjectInputStream(a);
+        return b.readObject();
+    }
+    
+    //sérialisation
+    public byte[] serialize(final Object obj) throws IOException {
+        ByteArrayOutputStream a = new ByteArrayOutputStream();
+        ObjectOutputStream b = new ObjectOutputStream(a);
+        b.writeObject(obj);
+        return a.toByteArray();
+    }
+    
 }
+
