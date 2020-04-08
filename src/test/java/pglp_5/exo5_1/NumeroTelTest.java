063cc06b2ccb50c5957831cfc6f06cd510e0a651
melissa bafdel <melissabafdel@gmail.com>
2020-04-08 18:02:20 +0200

    ajout de test

--- src/test/java/pglp_5/exo5_1/NumeroTelTest.java
+++ src/test/java/pglp_5/exo5_1/NumeroTelTest.java
@@ -4,9 +4,11 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,17 +22,34 @@ public class NumeroTelTest {
 	public void SetUp(){
 		numero = new NumeroTel("portable","0698552310");
 	}
-	
-	@Test
-	public void getDescriptif(){
-		String Type ="portable";
-		assertEquals(Type,numero.getDescriptif());
-	}
 	@Test
 	public void getNumeroTest(){
 		NumeroTel numero = new NumeroTel("portable","0698552310");
 		String num ="0698552310";
 		assertEquals(num, numero.getNumero());
+	}
+	@Test
+	public void getDescriptif(){
+		String Type ="portable";
+		assertEquals(Type,numero.getDescriptif());
+	}
+	
+	public void createTest()throws IOException, ClassNotFoundException{
+		String nomF = "NumeroTel";
+		File fichier = new File(nomF);
+		
+		DAOApp<NumeroTel> numTel = DaoFactory.getNumeroTelDAO();
+		numTel.create(numero);
+		File search = new File(nomF +".txt");
+		Object deserialisable =null;
+		 byte[] fileContent = Files.readAllBytes(search.toPath());
+		  deserialisable = deserialize(fileContent);
+		  NumeroTel type = (NumeroTel) deserialisable;
+		  assertTrue(fichier.exists());
+	        assertTrue(search.exists());
+	        assertEquals(type, numero);
+	        numTel.delete(numero);
+				
 	}
 	
 	private Object deserialize(final byte[] bytes) throws ClassNotFoundException, IOException {
@@ -59,5 +78,7 @@ public class NumeroTelTest {
         Assert.assertEquals(numero, deserialisable);
         Assert.assertEquals(numero, deserialisable2);
     }
+	
+	
 	
 }
